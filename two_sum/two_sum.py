26d36e02cdbde3bda20bf88d167bd2769c7b4a29
Huixiang Chen <chenhuixiang5319@gmail.com>
2016-06-22 15:24:08 -0400

    leetcode 1 2

---
+++ two_sum/two_sum.py
@@ -0,0 +1,17 @@
+#leetcode 1, two sum
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashTable = {}
+        for i in range(len(nums)):
+            hashTable[nums[i]] = i
+        
+        for i in range(len(nums)):
+            if target - nums[i] in hashTable and i != hashTable[target - nums[i]]:
+                return [i, hashTable[target-nums[i]]]
+        
+        return []
